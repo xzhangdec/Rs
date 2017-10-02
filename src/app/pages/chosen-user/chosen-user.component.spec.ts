b4761dcf6e325e6953b17aac54340984ed9e4f93
XI ZHANG <x7zhang@gmail.com>
2017-10-01 23:30:13 -0400

    delete unused module

--- src/app/pages/chosen-user/chosen-user.component.spec.ts
+++ src/app/pages/chosen-user/chosen-user.component.spec.ts
@@ -1,6 +1,6 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import {
-  async, fakeAsync, ComponentFixture, TestBed, tick, inject } from '@angular/core/testing';
+  async, ComponentFixture, TestBed } from '@angular/core/testing';
 import {
   RouterTestingModule
 } from '@angular/router/testing';
